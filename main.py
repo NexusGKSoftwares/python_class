3ae1b560816512004ec3490f83f6eaf5e4da3e24
NexusGKSoftwares <nexusgk3@gmail.com>
2025-08-25 22:56:11 +0300

    first class

---
+++ main.py
@@ -0,0 +1,68 @@
+# what is python programming language?
+# Python is a high-level, interpreted programming language known for its readability and versatility.
+# It is used in various fields such as web development, data analysis, artificial intelligence, scientific computing, and more.
+
+# installing python
+# You can download Python from the official website: https://www.python.org/downloads/
+# we installed the visual studio code editor to write and run python code.
+
+# verify python installation
+# Open a terminal or command prompt and type:
+# python --version
+# or
+# python3 --version
+
+# writing your first python program
+# print("Hello, World!")
+
+
+# running your python program
+# Save your code in a file named main.py and run it using the command:
+# python main.py
+# or
+# python3 main.py   
+
+#  1st Topic: Variables and Data Types & Input/Output Operations
+
+# 1. Variables and Data Types
+# Variables are used to store data in a program. Python supports various data types including:
+
+# name = "Alice"  
+# print(name)  
+
+# Data Types:
+# String: Text data, e.g., "Hello"
+# Integer: Whole numbers, e.g., 42
+# Float: Decimal numbers, e.g., 3.14
+# Boolean: True or False values, e.g., True
+
+# Example of different data types
+# name = "Alice"        # String
+# age = 30              # Integer
+# height = 5.5         # Float
+# is_student = False    # Boolean
+
+# print(name)
+# print(age)
+# print(height)
+# print(is_student)
+
+# Operations on Variables
+# You can perform various operations on variables, such as arithmetic operations for numbers and concatenation for strings.
+# three operators are
+# Arithmetic Operators: +, -, *, /, %
+# Comparison Operators: ==, !=, >, <, >=, <=
+# Logical Operators: and, or, not
+
+
+# explanation of operators
+# Arithmetic Operators
+#  it includes addition (+), subtraction (-), multiplication (*), division (/), and modulus (%).
+# Example:
+a = 10
+b = 3
+print(a + b)  # Addition
+print(a - b)  # Subtraction
+print(a * b)  # Multiplication
+print(a / b)  # Division
+print(a % b)  # Modulus
